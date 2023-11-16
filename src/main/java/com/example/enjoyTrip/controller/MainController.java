3845406a2986a8a6aa4d431150b8066b6d7d47f8
jongkook <whdrnrdl789@naver.com>
2023-11-16 09:13:25 +0900

    11.16

--- src/main/java/com/example/enjoyTrip/controller/MainController.java
+++ src/main/java/com/example/enjoyTrip/controller/MainController.java
@@ -1,15 +1,16 @@
 package com.example.enjoyTrip.controller;
 
-import com.example.enjoyTrip.dto.UserDto;
-import com.example.enjoyTrip.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.enjoyTrip.dto.UserDto;
+import com.example.enjoyTrip.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,7 +32,7 @@ public class MainController {
 
 	@PostMapping("/join")
 	@ResponseBody
-	public String join(UserDto user) {
+	public String join(@RequestBody UserDto user) {
 		user.setRole("ROLE_USER");
 		String rawPassword = user.getPassword();
 		// 시큐리티를 사용해서 로그인 할려면 암호화된 비밀번호를 사용해야함. 그래서 암호화 작업 필요
