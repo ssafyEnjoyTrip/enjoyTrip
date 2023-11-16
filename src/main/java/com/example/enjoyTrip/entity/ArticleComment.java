594c9f682fb1d7d242e49b37d6437f05d1bbd535
jongkook <whdrnrdl789@naver.com>
2023-11-16 10:14:31 +0900

    Entity Article Comment

--- src/main/java/com/example/enjoyTrip/entity/ArticleComment.java
+++ src/main/java/com/example/enjoyTrip/entity/ArticleComment.java
@@ -24,72 +24,28 @@ public class ArticleComment {
     @javax.persistence.Column(name = "comment_id")
     private int commentId;
 
-    public int getCommentId() {
-        return commentId;
-    }
-
-    public void setCommentId(int commentId) {
-        this.commentId = commentId;
-    }
-
     @Basic
     @Column(name = "article_id")
     private int articleId;
-
-    public int getArticleId() {
-        return articleId;
-    }
-
-    public void setArticleId(int articleId) {
-        this.articleId = articleId;
-    }
 
     @Basic
     @Column(name = "user_id")
     private int userId;
 
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
     @Basic
     @Column(name = "comment")
     private String comment;
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Basic
-    @Column(name = "member_id")
-    private Integer memberId;
-
-    public Integer getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Integer memberId) {
-        this.memberId = memberId;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ArticleComment that = (ArticleComment) o;
-        return commentId == that.commentId && articleId == that.articleId && userId == that.userId && Objects.equals(comment, that.comment) && Objects.equals(memberId, that.memberId);
+        return commentId == that.commentId && articleId == that.articleId && userId == that.userId && Objects.equals(comment, that.comment) ;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(commentId, articleId, userId, comment, memberId);
+        return Objects.hash(commentId, articleId, userId, comment);
     }
 }
