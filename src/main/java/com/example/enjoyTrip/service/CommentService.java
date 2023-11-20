ea03b13b0ee429f42f530bac55aa08ee4af4c2c9
jongkook <whdrnrdl789@naver.com>
2023-11-20 14:51:34 +0900

    댓글, 관광지

--- src/main/java/com/example/enjoyTrip/service/CommentService.java
+++ src/main/java/com/example/enjoyTrip/service/CommentService.java
@@ -7,5 +7,4 @@ import java.util.List;
 public interface CommentService {
     List<ArticleComment> findAll();
     List<ArticleComment>  findByArticleArticleId(int articleId);
-    ArticleComment save(ArticleComment comment);
 }
